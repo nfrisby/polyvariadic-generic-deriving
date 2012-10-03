9ca6ecbe094ccd5abe9ca7f815545a9d596191b5
Nicolas Frisby <nicolas.frisby@gmail.com>
2012-10-03 12:12:28 -0500

    added generic Enumeration and a couple simpler example types

---
+++ Data/PVGD/Enumerate.hs
@@ -0,0 +1,101 @@
+{-# LANGUAGE TypeFamilies, LambdaCase, PolyKinds, TypeOperators, DataKinds,
+  FlexibleContexts, MultiParamTypeClasses, GADTs, FlexibleInstances,
+  ScopedTypeVariables, UndecidableInstances, InstanceSigs, DefaultSignatures #-}
+
+module Data.PVGD.Enumerate where
+
+import Data.PVGD.BasicTypes
+import Data.PVGD.W
+import Data.PVGD.View
+
+
+
+interleave x y = diag [x, y]
+
+-- plucked from Luke Palmer's Control.Monad.Omega
+diag :: [[a]] -> [a]
+diag = concat . stripe
+    where
+    stripe [] = []
+    stripe ([]:xss) = stripe xss
+    stripe ((x:xs):xss) = [x] : zipCons xs (stripe xss)
+
+    zipCons [] ys = ys
+    zipCons xs [] = map (:[]) xs
+    zipCons (x:xs) (y:ys) = (x:y) : zipCons xs ys
+
+
+
+
+
+-- the enumerations being extended
+data family Enums (n :: Nat) :: [*] -> *
+data instance Enums Z ps = Enums
+-- use a GADT so that the provided functions can determine @ps@ and @ps'@
+infixl 5 :::
+data instance Enums (S n) (ps :: [*]) :: * where
+  (:::) :: (ps ~ (a ': as)) => Enums n as -> [a] -> Enums (S n) ps
+
+
+
+
+
+
+class Enumerate (t :: k) where
+  enum :: NLong (CountArgs ('KindProxy :: KindProxy k)) ps =>
+          Enums (CountArgs ('KindProxy :: KindProxy k)) ps -> [W t ps]
+
+  default enum :: (NLong (CountArgs ('KindProxy :: KindProxy k)) ps,
+                   Generic t, EnumerateR (Rep t) (CountArgs ('KindProxy :: KindProxy k))) =>
+                  Enums (CountArgs ('KindProxy :: KindProxy k)) ps -> [W (t :: k) ps]
+  enum = gen_enum
+
+class EnumerateR (t :: [*] -> *) (n :: Nat) where
+  enumR :: NLong n ps => Enums n ps -> [t ps]
+
+gen_enum :: (NLong (CountArgs ('KindProxy :: KindProxy k)) ps,
+             Generic t, EnumerateR (Rep t) (CountArgs ('KindProxy :: KindProxy k))) =>
+             Enums (CountArgs ('KindProxy :: KindProxy k)) ps -> [W (t :: k) ps]
+gen_enum = map frRep . enumR
+
+instance (EnumerateR l n, EnumerateR r n) => EnumerateR (l :+: r) n where
+  enumR enums = map L (enumR enums) `interleave` map R (enumR enums)
+instance EnumerateR Void n where enumR _ = []
+
+instance (EnumerateR l n, EnumerateR r n) => EnumerateR (l :*: r) n where
+  enumR enums = diag (map (\x -> map (\y -> x:*:y) $ enumR enums ) $ enumR enums)
+--  enumR enums = diag $
+--                flip map (enumR enums) $ \l ->
+--                flip map (enumR enums) $ \r ->
+--                  l :*: r
+instance EnumerateR U n where enumR _ = [U]
+
+
+
+instance NthEnumR m n => EnumerateR (Par m) n where
+  enumR = map Par . nthEnumR (Proxy :: Proxy m)
+
+class NthEnumR m n where nthEnumR :: Proxy m -> Enums n ps -> [Nth m ps]
+
+instance (n ~ S n') => NthEnumR Z n where
+  nthEnumR _ (_ ::: as) = as
+instance (n ~ S n', NthEnumR m n') => NthEnumR (S m) n where
+  nthEnumR _ (enums ::: _) = nthEnumR (Proxy :: Proxy m) enums
+
+
+
+instance (Lemma_NLongLengthZAP argReps,
+          Enumerate t, NewEnums argReps n,
+          CountArgs ('KindProxy :: KindProxy k) ~ Length argReps) =>
+  EnumerateR (T (t :: k) argReps) n where
+  enumR :: forall (ps :: [*]). NLong n ps => Enums n ps -> [T t argReps ps]
+  enumR = lemma_NLongLengthZAP (Proxy :: Proxy argReps) (Proxy :: Proxy ps) $
+          map T . enum . newEnums (Proxy :: Proxy argReps)
+
+class NewEnums (argReps :: [[*] -> *]) (n :: Nat) where
+  newEnums :: NLong n ps => Proxy argReps -> Enums n ps -> Enums (Length argReps) (ZAP argReps ps)
+
+instance NewEnums '[] n where newEnums _ _ = Enums
+instance (EnumerateR argRep n, NewEnums argReps n) =>
+  NewEnums (argRep ': argReps) n where
+  newEnums _ enums = newEnums (Proxy :: Proxy argReps) enums ::: enumR enums
--- examples/Examples.hs
+++ examples/Examples.hs
@@ -8,7 +8,7 @@ module Examples where
 import Data.PVGD.BasicTypes (Nat(S, Z))
 
 -- the tricky W data family
-import Data.PVGD.W (W(W0, W1, W2), unW1, unW2, asW1, asW2)
+import Data.PVGD.W (W(W0, W1, W2), unW0, unW1, unW2, asW1, asW2)
 
 -- the generic view
 import Data.PVGD.View
@@ -16,15 +16,22 @@ import Data.PVGD.View
    U(U), (:*:)((:*:)), (:+:)(L, R), foldPlus, Par(Par), unPar,
    T(T), unT)
 
--- the interesting T representation type and the Covariant class with a generic
--- definition
-import Data.PVGD.Covariant (Covariant, covmap, Maps(Maps, (:::)))
+-- the Covariant class with a generic definition
+import Data.PVGD.Covariant (Covariant, covmap, Maps(Maps))
+import qualified Data.PVGD.Covariant as Covariant
+
+-- the Enumerate class with a generic definition
+import Data.PVGD.Enumerate (Enumerate, enum, Enums(Enums))
+import qualified Data.PVGD.Enumerate as Enumerate
 
 
 
 --------------------
 -- representations for simple types: non-recursive with only parameters as
 -- fields
+type instance Rep () = U
+instance Generic () where toRep _ = U; frRep _ = W0 ()
+
 type instance Rep Bool = U :+: U
 instance Generic Bool where
   toRep (W0 b) = if b then L U else R U
@@ -53,11 +60,19 @@ instance Generic [] where
   frRep (L U) = W1 []
   frRep (R (Par a :*: T (W1 as))) = W1 $ a : map unPar as
 
+instance Covariant () -- NB non-sensical, but well-typed
 instance Covariant Bool -- NB non-sensical, but well-typed
 instance Covariant Maybe
 instance Covariant (,)
 instance Covariant Either
 instance Covariant []
+
+instance Enumerate ()
+instance Enumerate Bool
+instance Enumerate Maybe
+instance Enumerate (,)
+instance Enumerate Either
+instance Enumerate []
 
 
 
@@ -79,6 +94,35 @@ gen_eq2 x y = toRep (W2 x) == toRep (W2 y)   where _ = x `asTypeOf` y
 
 
 --------------------
+-- some nested applications
+
+data Ex1 (b :: *) (a :: *) = Ex1 [Either a b] deriving Show
+
+type instance Rep Ex1 = T [] '[T Either '[Par (S Z), Par Z]]
+instance Generic Ex1 where
+  toRep (W2 (Ex1 x)) = T $ W1 $ map (T . covmap (Maps Covariant.::: Par Covariant.::: Par) . W2) x
+  frRep = W2 . Ex1 . map (unW2 . covmap (Maps Covariant.::: unPar Covariant.::: unPar) . unT) . unW1 . unT
+
+instance (Eq b, Eq a) => Eq (Ex1 b a) where (==) = gen_eq2
+instance Covariant Ex1
+instance Enumerate Ex1
+
+
+data Ex2 (b :: *) (a :: *) = Ex2 (Either [a] b) deriving Show
+
+type instance Rep Ex2 = T Either '[Par (S Z), T [] '[Par Z]]
+instance Generic Ex2 where
+  toRep (W2 (Ex2 x)) = T $ covmap (Maps Covariant.::: (T . W1 . map Par) Covariant.::: Par) $ W2 x
+  frRep = W2 . Ex2 . unW2 . covmap (Maps Covariant.::: (map unPar . unW1 . unT) Covariant.::: unPar) . unT
+
+instance (Eq b, Eq a) => Eq (Ex2 b a) where (==) = gen_eq2
+instance Covariant Ex2
+instance Enumerate Ex2
+
+
+
+
+--------------------
 -- X involves a type application and has two parameters
 
 data X f (b :: *) (a :: *) = X Int (f a b) deriving Show
@@ -87,12 +131,16 @@ data X f (b :: *) (a :: *) = X Int (f a b) deriving Show
 type instance Rep (X f) = T Int '[] :*: T f '[Par (S Z), Par Z]
 instance Covariant f => Generic (X f) where
   toRep (W2 (X i fba)) =
-    T (W0 i) :*: T (covmap (Maps ::: Par ::: Par) $ W2 fba)
+    T (W0 i) :*: T (covmap (Maps Covariant.::: Par Covariant.::: Par) $ W2 fba)
   frRep (T (W0 i) :*: T fba) =
-    W2 $ X i $ unW2 $ covmap (Maps ::: unPar ::: unPar) fba
+    W2 $ X i $ unW2 $ covmap (Maps Covariant.::: unPar Covariant.::: unPar) fba
 
 instance Covariant Int where covmap Maps (W0 x) = W0 x
 instance Covariant f => Covariant (X f)
+
+instance Enumerate Int where enum Enums = map W0 $ Enumerate.interleave [0, -1..] [1,2..]
+instance Enumerate Char where enum Enums = map W0 $ [minBound..maxBound]
+instance (Covariant f, Enumerate f) => Enumerate (X f)
 
 instance (Covariant f, Eq (Rep (X f) '[a, b])) => Eq (X f b a) where (==) = gen_eq2
 
@@ -105,17 +153,19 @@ newtype Flop (b :: *) (a :: *) = Flop (Flip a b) deriving Show
 type instance Rep Flip = Par (S Z) :*: Par Z   :+:   T Flop '[Par (S Z), Par Z]
 instance Generic Flip where
   toRep (W2 (Stop b a)) = L $ Par b :*: Par a
-  toRep (W2 (Flip x))   = R $ T $ covmap (Maps ::: Par ::: Par) $ W2 x
+  toRep (W2 (Flip x))   = R $ T $ covmap (Maps Covariant.::: Par Covariant.::: Par) $ W2 x
   frRep (L (Par b :*: Par a)) = W2 $ Stop b a
-  frRep (R (T x)) = W2 $ Flip $ unW2 $ covmap (Maps ::: unPar ::: unPar) x
+  frRep (R (T x)) = W2 $ Flip $ unW2 $ covmap (Maps Covariant.::: unPar Covariant.::: unPar) x
 
 type instance Rep Flop = T Flip '[Par (S Z), Par Z]
 instance Generic Flop where
-  toRep (W2 (Flop x)) = T $ covmap (Maps ::: Par ::: Par) $ W2 x
-  frRep = W2 . Flop . unW2 . covmap (Maps ::: unPar ::: unPar) . unT
+  toRep (W2 (Flop x)) = T $ covmap (Maps Covariant.::: Par Covariant.::: Par) $ W2 x
+  frRep = W2 . Flop . unW2 . covmap (Maps Covariant.::: unPar Covariant.::: unPar) . unT
 
 instance Covariant Flip
 instance Covariant Flop
+instance Enumerate Flip
+instance Enumerate Flop
 
 instance (Eq b, Eq a) => Eq (Flip b a) where (==) = gen_eq2
 instance (Eq b, Eq a) => Eq (Flop b a) where (==) = gen_eq2
@@ -125,9 +175,16 @@ instance (Eq b, Eq a) => Eq (Flop b a) where (==) = gen_eq2
 
 
 example0   = X 0 ("foo", True) == X 0 ("bar", False)
-example1 f = covmap (Maps ::: f ::: id) `asW2` X 0 ((), ["foo", "baz", "bar"])
-example2   = covmap (Maps ::: reverse ::: map (+1)) `asW2` ("foo", [0..5])
+example1 f = covmap (Maps Covariant.::: f Covariant.::: id) `asW2` X 0 ((), ["foo", "baz", "bar"])
+example2   = covmap (Maps Covariant.::: reverse Covariant.::: map (+1)) `asW2` ("foo", [0..5])
 example3   = covmap (error "testing inference") `asW1` [0..5] -- inferred :: [t]
 example4   =
    (==) (Flip $ Flop $ Flip $ Flop $ Stop False "oof") $
-   covmap (Maps ::: not ::: reverse) `asW2` (Flip $ Flop $ Flip $ Flop $ Stop True "foo")
+   covmap (Maps Covariant.::: not Covariant.::: reverse) `asW2` (Flip $ Flop $ Flip $ Flop $ Stop True "foo")
+
+example5 = map unW0 $ enum Enums :: [Bool]
+example6 = map unW1 $ enum (Enums Enumerate.::: map unW0 (enum Enums)) :: [[Bool]]
+example7 = map unW2 $ enum (Enums Enumerate.::: map unW0 (enum Enums) Enumerate.::: map unW0 (enum Enums)) :: [(,) Bool Bool]
+example8 = map unW2 $ enum (Enums Enumerate.::: map unW0 (enum Enums) Enumerate.::: map unW0 (enum Enums)) :: [X (,) Bool Bool]
+example9 = map unW2 $ enum (Enums Enumerate.::: map unW0 (enum Enums) Enumerate.::: map unW0 (enum Enums)) :: [Flip Bool Bool]
+example10 = map unW2 $ enum (Enums Enumerate.::: map unW0 (enum Enums) Enumerate.::: map unW0 (enum Enums)) :: [X Either () Bool]
